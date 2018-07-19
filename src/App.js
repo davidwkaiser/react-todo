3dddec18cbeee6a6181f6ef2cb02bd45bd8eff79
davidwkaiser <davidwkaiser@yahoo.com>
2018-07-18 23:14:44 -0500

    not working, but i'm tired

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ import AddBox from './AddBox'
 import logo from './logo.svg';
 import './App.css';
 
+const TODOS = [
+{action:"Pet the cat"},
+{action:"Deposit paycheck"},
+{action:"Water the driveway"}
+];
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
@@ -13,11 +20,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-    //    <div className="App-intro">
           <Title/>
-          <MainList/>
+          <MainList todos={TODOS} />
           <AddBox/>
-        </div>
       </div>
     );
   }
--- src/MainList.js
+++ src/MainList.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import './MainList.css';
 
 class MainList extends Component {
+  constructor(props){
+    super(props);
+  };
+
+  createRows = function(){
+    let rows = "<div className='piece'><p>Here is the List</p></div>";
+    for(var i =0; i<this.props.todos.length; i++){
+        var thing = this.props.todos.length[i]
+    // this.props.todos.forEach(function(todo) {
+        rows += "<p className='item'>{thing.action}</p>"
+      }
+      rows += "</div>"
+    return rows;
+  };
+
   render(){
     return (
-      <div className="piece"><p>This is where the list will go</p></div>
-      );
+      // <div className="piece"><p>Here is the List</p></div>
+      this.createRows()
+      )
   }
 }
 
